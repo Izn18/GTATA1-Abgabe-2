28abd623cf6a736f35555ea75252947ae65765c3
Izn18 <adil_a@web.de>
2020-06-06 18:36:25 +0200

    First Commit

--- Assets/2_Scripts/Movement.cs
+++ Assets/2_Scripts/Movement.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 10f;
+    public CharacterController controller;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // LimitPosition(); 
+    }
+
+    ///<summary>
+    /// This method makes sure that the Player stays in a given area
+    ///</summary>
+    public void LimitPosition()
+    {
+        float currX = transform.position.x;
+        float currY = transform.position.y;
+        float currZ = transform.position.z;
+
+        // Limit the position on the y-axis
+        if (currY > 10)
+        {
+            transform.position = new Vector3(currX, 10, currZ);
+        }
+
+        // Limit the position on the z-axis
+        if (currZ > 23)
+        {
+            transform.position = new Vector3(currX, currY, 23);
+        }
+
+        if (currZ < -17)
+        {
+            transform.position = new Vector3(currX, currY, -17);
+        }
+
+        // Limit the position on the x-axis
+        if (currX > 23)
+        {
+            transform.position = new Vector3(23, currY, currZ);
+        }
+
+        if (currX < -17)
+        {
+            transform.position = new Vector3(-17, currY, -currZ);
+        }
     }
 }
